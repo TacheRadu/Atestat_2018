1b6423f14d5f31822fee337280fd4a4306a62a99
Stefan Stefanel <bd.stefanspiridon@gmail.com>
2018-11-08 07:53:20 +0200

    Design by Stephen

--- Login/Login/Form1.Designer.cs
+++ Login/Login/Form1.Designer.cs
@@ -28,20 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(20)))), ((int)(((byte)(89)))));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(275, 143);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(20)))), ((int)(((byte)(89)))));
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(520, 143);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 28);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(20)))), ((int)(((byte)(89)))));
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button3.Location = new System.Drawing.Point(365, 216);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(178, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(18)))), ((int)(((byte)(92)))));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Image = global::Login.Properties.Resources.download__2_;
+            this.pictureBox1.Location = new System.Drawing.Point(338, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(257, 88);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(20)))), ((int)(((byte)(89)))));
+            this.button4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button4.Location = new System.Drawing.Point(29, 276);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 62);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(54)))), ((int)(((byte)(59)))));
+            this.ClientSize = new System.Drawing.Size(914, 359);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Location = new System.Drawing.Point(458, 0);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- Login/Login/Form1.cs
+++ Login/Login/Form1.cs
@@ -1,29 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Data;
-using System.Drawing;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
 using System.Windows.Forms;
-using MySql.Data.MySqlClient;
 
-namespace Login{
+namespace Login
+{
     public partial class Form1 : Form
     {
-        private Button submit = new Button();
-        private Button support = new Button();
-        private TextBox user = new TextBox();
-        private TextBox pass = new TextBox();
-        private void buttonPress(object sender, EventArgs e)
-        {
-            TextBox box = sender as TextBox;
-            if (box.Text == "username" || box.Text == "password") {
-                box.Clear();
-            }
-        }
         private void submission(object sender, EventArgs e)
         {
             if (user.Text == "")
@@ -70,35 +55,22 @@ namespace Login{
         public Form1()
         {
             InitializeComponent();
-            user.Text = "username";
-            pass.Text = "password";
-            user.Size = new System.Drawing.Size(70, 30);
-            user.Left = 50;
-            user.Top = 50;
-            pass.Size = new System.Drawing.Size(70, 30);
-            pass.Left = (this.Width - 100 - pass.Width) / 2;
-            pass.Top = 50;
-            pass.PasswordChar = '*';
-            submit.Size = new Size(70, 30);
-            submit.Top = 50;
-            submit.Left = this.Width - 100;
-            submit.Text = "Login";
-            support.Size = new Size(100, 100);
-            support.Left = this.Width / 2 - 50;
-            support.Top = 200;
-            support.Text = "Support";
-            support.Click += new EventHandler(goToSupport);
-            //submit.ForeColor = Color.Blue;
-            submit.BackColor = Color.RoyalBlue;
-            submit.Show();
-            user.Click += new EventHandler(buttonPress);
-            pass.Click += new EventHandler(buttonPress);
-            submit.Click += new EventHandler(submission);
-            this.Controls.Add(user);
-            this.Controls.Add(pass);
-            this.Controls.Add(submit);
-            this.Controls.Add(support);
-            this.BackColor = Color.FromArgb(255, 48, 48, 48);
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
     public class ConnectDB
