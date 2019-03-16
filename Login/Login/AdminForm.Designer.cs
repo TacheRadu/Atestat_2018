410207b940a16d770a21aff6755791baa7c62523
Radu Tache <radutache76@gmail.com>
2019-03-16 15:02:50 +0200

    Going to be a side-menu here..

--- Login/Login/AdminForm.Designer.cs
+++ Login/Login/AdminForm.Designer.cs
@@ -34,6 +34,7 @@ namespace Login
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.usersBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.updateBtn = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.usersBindingSource)).BeginInit();
             this.SuspendLayout();
@@ -41,9 +42,9 @@ namespace Login
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Location = new System.Drawing.Point(100, 0);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(800, 291);
+            this.dataGridView1.Size = new System.Drawing.Size(730, 410);
             this.dataGridView1.TabIndex = 1;
             // 
             // usersBindingSource
@@ -53,18 +54,27 @@ namespace Login
             // updateBtn
             // 
             this.updateBtn.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.updateBtn.Location = new System.Drawing.Point(392, 415);
+            this.updateBtn.Location = new System.Drawing.Point(410, 430);
             this.updateBtn.Name = "updateBtn";
             this.updateBtn.Size = new System.Drawing.Size(75, 23);
             this.updateBtn.TabIndex = 3;
             this.updateBtn.Text = "Update";
             this.updateBtn.UseVisualStyleBackColor = true;
             // 
+            // treeView1
+            // 
+            this.treeView1.BackColor = System.Drawing.Color.Azure;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(100, 410);
+            this.treeView1.TabIndex = 4;
+            // 
             // AdminForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(834, 461);
+            this.Controls.Add(this.treeView1);
             this.Controls.Add(this.updateBtn);
             this.Controls.Add(this.dataGridView1);
             this.Name = "AdminForm";
@@ -80,5 +90,6 @@ namespace Login
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.BindingSource usersBindingSource;
         private System.Windows.Forms.Button updateBtn;
+        private System.Windows.Forms.TreeView treeView1;
     }
 }
