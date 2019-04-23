8a28461843efc8c798b341f610c86f67247aac98
Radu Tache <radutache76@gmail.com>
2019-04-23 08:14:22 +0300

    Added intuitive chromatics on rows state

--- Login/Login/AdminForm.cs
+++ Login/Login/AdminForm.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace Login
 {
@@ -41,13 +42,38 @@ namespace Login
         private async Task<DataGridView> GetGrid(string s)
         {
             DataGridView grid = new DataGridView();
+            grid.UserDeletingRow += Grid_UserDeletingRow;
+            grid.UserAddedRow += Grid_UserAddedRow;
+            grid.CellValueChanged += Grid_CellValueChanged;
             grid.Dock = DockStyle.Fill;
-            var source = JsonConvert.DeserializeObject(await client.GetStringAsync("http://localhost:3000/gettable?table=" + s));
+            var source = JsonConvert.DeserializeObject<DataTable>(await client.GetStringAsync("http://localhost:3000/gettable?table=" + s));
             grid.DataSource = source;
             return grid;
         }
 
+        private void Grid_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridView dgv = sender as DataGridView;
+            if(dgv.Rows[e.RowIndex].DefaultCellStyle.BackColor != Color.LightGreen)
+            {
+                dgv.Rows[e.RowIndex].DefaultCellStyle.BackColor = Color.Khaki;
+            }
+        }
 
+        private void Grid_UserAddedRow(object sender, DataGridViewRowEventArgs e)
+        {
+            e.Row.DataGridView.Rows[e.Row.DataGridView.NewRowIndex - 1].DefaultCellStyle.BackColor = Color.LightGreen;
+        }
+
+        private void Grid_UserDeletingRow(object sender, DataGridViewRowCancelEventArgs e)
+        {
+            if (e.Row.DefaultCellStyle.BackColor != Color.LightGreen)
+            {
+                e.Row.DefaultCellStyle.BackColor = Color.LightCoral;
+                e.Cancel = true;
+            }
+
+        }
 
         private async Task<TabPage> getTab(string s)
         {
